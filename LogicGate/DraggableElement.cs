e539aa47428e863522b7c28f903acea79e465724
Rygasary <44134904+Rygasary@users.noreply.github.com>
2023-11-12 23:51:42 +0100

    Initial commit

---
+++ LogicGate/DraggableElement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LogicGate
+{
+    internal class DraggableElement
+    {
+        public readonly MainWindow window;
+        public readonly Ellipse ellipse;
+        public DraggableElement(MainWindow _window)
+        {
+            ellipse = new Ellipse();
+            ellipse.Fill = Brushes.Red;
+            ellipse.Height = 100;
+            ellipse.Width = 100;
+            this.window = _window;
+            ellipse.PreviewMouseDown += OnElementSelected;
+            _window.CanvasMain.Children.Add(ellipse);
+            Canvas.SetTop(ellipse, 0);
+            Canvas.SetLeft(ellipse, 0);
+        }
+
+        private void OnElementSelected(object _sender, MouseButtonEventArgs _e)
+        {
+            window.currentObjectOffset = _e.GetPosition(window.CanvasMain);
+            window.currentObjectOffset.Y -= Canvas.GetTop(ellipse);
+            window.currentObjectOffset.X -= Canvas.GetLeft(ellipse);
+            window.OnMouseMoveEvent += OnMoveAround;
+            window.OnMouseUpEvent += OnUnselect;
+            window.CanvasMain.CaptureMouse();
+        }
+
+        private void OnMoveAround(object _sender, MouseEventArgs _e)
+        {
+            Point _pos = _e.GetPosition(_sender as IInputElement);
+            Canvas.SetTop(ellipse, _pos.Y - window.currentObjectOffset.Y);
+            Canvas.SetLeft(ellipse, _pos.X - window.currentObjectOffset.X);
+        }
+
+        private void OnUnselect(object _sender, MouseButtonEventArgs _e)
+        {
+            window.CanvasMain.ReleaseMouseCapture();
+            window.OnMouseMoveEvent -= OnMoveAround;
+            window.OnMouseUpEvent -= OnUnselect;
+        }
+    }
+}
---
+++ LogicGate/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogicGate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Action<object, MouseEventArgs> OnMouseMoveEvent;
+        public Action<object, MouseButtonEventArgs> OnMouseUpEvent;
+        public Point currentObjectOffset = new(0,0);
+        public MainWindow()
+        {
+            InitializeComponent();
+            DraggableElement _de = new(this);
+        }
+
+        private void CanvasOnMouseMove(object sender, MouseEventArgs e)
+        {
+            OnMouseMoveEvent?.Invoke(sender, e);
+        }
+
+        private void CanvasOnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            OnMouseUpEvent?.Invoke(sender, e);
+        }
+    }
+}
